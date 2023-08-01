a66c214afa2bb913d5ba503c8f82187b3902b469
LUCY <wangecikariu.luc@gmail.com>
2023-08-01 23:12:07 +0300

    Dom Lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+// CREATING DOM ELEMENT
+
+const main = document.querySelector('#main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.innerHTML = '<h1>LUCY is the champion</h1>';
+
+//SET ATTRIBUTE
+newHeader.setAttribute('id', 'victory');
